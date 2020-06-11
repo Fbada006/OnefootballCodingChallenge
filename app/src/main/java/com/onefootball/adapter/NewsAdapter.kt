570ad547de22ef507dd178117808252138fe56e2
Fbada006 <ferdinandkamata@yahoo.com>
2020-06-11 20:22:54 +0300

    refactor: cleanup NewsAdapter.kt to be more concise

--- app/src/main/java/com/onefootball/adapter/NewsAdapter.kt
+++ app/src/main/java/com/onefootball/adapter/NewsAdapter.kt
@@ -5,44 +5,48 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.onefootball.R
 import com.onefootball.model.News
 import com.onefootball.utils.NewsDiffCallback
+import kotlinx.android.synthetic.main.news_item.view.*
 
 /**The [ListAdapter] is a better implementation because it also computes diffs between lists
  * all in the background using the [NewsDiffCallback]
  * */
 class NewsAdapter : ListAdapter<News, NewsAdapter.NewsViewHolder>(NewsDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
-        return NewsViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder =
+        NewsViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val news = getItem(position)
-
-        holder.titleView.text = news.title
-        holder.newsView.load(uri = news.imageURL)
-        holder.resourceImage.load(uri = news.resourceURL)
-        holder.resourceName.text = news.resourceName
-        holder.itemView.setOnClickListener {
-            it.context.startActivity(
-                Intent(Intent.ACTION_VIEW, Uri.parse(news.newsLink))
-            )
-        }
+        holder.bind(getItem(position))
     }
 
     class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        var titleView: TextView = itemView.findViewById(R.id.news_title)
-        var newsView: ImageView = itemView.findViewById(R.id.news_view)
-        var resourceImage: ImageView = itemView.findViewById(R.id.resource_icon)
-        var resourceName: TextView = itemView.findViewById(R.id.resource_name)
+        //Bind views to data
+        fun bind(news: News) {
+            itemView.news_title.text = news.title
+            itemView.news_view.load(uri = news.imageURL)
+            itemView.resource_icon.load(uri = news.resourceURL)
+            itemView.resource_name.text = news.resourceName
+            itemView.setOnClickListener {
+                it.context.startActivity(
+                    Intent(Intent.ACTION_VIEW, Uri.parse(news.newsLink))
+                )
+            }
+        }
+
+        //For inflating the layout in onCreateViewHolder()
+        companion object {
+            fun from(parent: ViewGroup): NewsViewHolder {
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+                return NewsViewHolder(view)
+            }
+        }
     }
 }
